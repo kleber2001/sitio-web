5ecbcd566564b70ae7dd20a18ffb7426410afe3a
Kleber Nuñez <kgnunez@espol.edu.ec>
2023-01-25 04:17:48 -0500

    actualizacion

--- server.js
+++ server.js
@@ -19,8 +19,23 @@ app.get('/', (req, res) => {
     res.end('Bienvenidos al servidor backend Node.js. Corriendo...')
 })
 
+// usar estáticos cuando esta en modo produccion //
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get("*", (req, res) => {
+        res.sendFile((__dirname + "/frontend/build/index.html"));
+    })
+    }
+    
+    
+    // cambio de puerto en heroku
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+    port = 5000;
+    }  
+
 
 //Configurar server básico
-app.listen(5000, function(){
+app.listen(port, function(){
     console.log('El servidor NODE está corriendo correctamente')
 })
